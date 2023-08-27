c240581bfe9f6e39e4c976472bb44d14a7dfe292
SAaASs <andrewcraft789@gmail.com>
2023-08-27 11:17:17 +0300

    test2

--- controllers/cards.js
+++ controllers/cards.js
@@ -17,15 +17,15 @@ module.exports.unlikeCard = (req, res) => {
 };
 
 module.exports.getAllCards = (req, res) => {
-  Card.find({}).then((card) => res.send({ data: card })).catch((err) => res.status(500).send({ message: `Произошла ошибка${err}` }));
+  Card.find({}).then((card) => res.send({ data: card })).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
 };
 
 module.exports.deleteCardById = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId).then((card) => res.send({ data: card })).catch((err) => res.status(500).send({ message: `Произошла ошибка${err}` }));
+  Card.findByIdAndRemove(req.params.cardId).then((card) => res.send({ data: card })).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
-  Card.create({ name, link, owner }).then((sm) => { res.send(`${sm}`); }).catch((err) => res.status(500).send({ message: `Произошла ошибка${err}` }));
+  Card.create({ name, link, owner }).then((sm) => { res.send(`${sm}`); }).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
 };
