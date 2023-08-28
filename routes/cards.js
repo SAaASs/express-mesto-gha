6c097f29a2b7dd2c083a8eaa9b1602aad224bb69
SAaASs <andrewcraft789@gmail.com>
2023-08-28 12:11:21 +0300

    another routes fix try

--- routes/cards.js
+++ routes/cards.js
@@ -3,10 +3,10 @@ const {
   likeCard, unlikeCard, getAllCards, deleteCardById, createCard,
 } = require('../controllers/cards');
 
-cardsRouter.get('/', getAllCards);
-cardsRouter.delete('/:cardId', deleteCardById);
-cardsRouter.post('/', createCard);
-cardsRouter.put('/:cardId/likes', likeCard);
-cardsRouter.delete('/:cardId/likes', unlikeCard);
+cardsRouter.get('/cards/', getAllCards);
+cardsRouter.delete('/cards/:cardId', deleteCardById);
+cardsRouter.post('/cards/', createCard);
+cardsRouter.put('/cards/:cardId/likes', likeCard);
+cardsRouter.delete('/cards/:cardId/likes', unlikeCard);
 
 module.exports = { cardsRouter };
