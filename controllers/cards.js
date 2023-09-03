2ddfe344a0c2c3029259a77f3d81d023ae1717ca
SAaASs <andrewcraft789@gmail.com>
2023-09-03 16:41:26 +0300

    lastFixAgain

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,33 +1,88 @@
-const Mongoose = require('mongoose');
-const Card = require('../models/card');
+const Mongoose = require("mongoose");
+const Card = require("../models/card");
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true },
-  ).then((card) => { if (card == null) { const er = new Error(card); res.status(404).send({ message: er.message }); } else { res.send(card); } }).catch((err) => { if (err instanceof Mongoose.CastError) { const er = new Error(err); console.log(er); res.status(400).send({ message: er.message }); } });
+    { new: true }
+  )
+    .then((card) => {
+      if (card == null) {
+        const er = new Error(card);
+        res.status(404).send({ message: er.message });
+      } else {
+        res.send(card);
+      }
+    })
+    .catch((err) => {
+      if (err instanceof Mongoose.CastError) {
+        const er = new Error(err);
+        console.log(er);
+        res.status(400).send({ message: er.message });
+      }
+    });
 };
 
 module.exports.unlikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
-    { new: true },
-  ).then((card) => { if (card == null) { const er = new Error(card); res.status(404).send({ message: er.message }); } else { res.send(card); } }).catch((err) => { if (err instanceof Mongoose.CastError) { const er = new Error(err); console.log(er); res.status(400).send({ message: er.message }); } });
+    { new: true }
+  )
+    .then((card) => {
+      if (card == null) {
+        const er = new Error(card);
+        res.status(404).send({ message: er.message });
+      } else {
+        res.send(card);
+      }
+    })
+    .catch((err) => {
+      if (err instanceof Mongoose.CastError) {
+        const er = new Error(err);
+        console.log(er);
+        res.status(400).send({ message: er.message });
+      }
+    });
 };
 
 module.exports.getAllCards = (req, res) => {
-  Card.find({}).then((card) => res.send({ data: card })).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
+  Card.find({})
+    .then((card) => res.send({ data: card }))
+    .catch((err) =>
+      res.status(400).send({ message: `Произошла ошибка${err}` })
+    );
 };
 
 module.exports.deleteCardById = (req, res) => {
   const sees = req.params.cardId;
-  Card.findByIdAndRemove(sees).then((card) => { if (card == null) { const er = new Error(card); res.status(404).send({ message: er.message }); } else { res.send(card); } }).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
+  Card.findByIdAndRemove(sees)
+    .then((card) => {
+      if (card == null) {
+        const er = new Error(card);
+        res.status(404).send({ message: er.message });
+      } else {
+        res.send(card);
+      }
+    })
+    .catch((err) =>
+      res.status(400).send({ message: `Произошла ошибка${err}` })
+    );
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
-  Card.create([{ name, link, owner }], { runValidators: true }).then((card) => { res.send(card[0]); }).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
+  Card.create([{ name, link, owner }], { runValidators: true })
+    .then((card) => {
+      res.send(card[0]);
+    })
+    .catch((err) => {
+      if (err.errors.name.name == "ValidatorError") {
+        res.status(400).send({ message: err });
+        return;
+      }
+      res.status(500).send(err);
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,52 @@
-const Mongoose = require('mongoose');
-const User = require('../models/user');
+const Mongoose = require("mongoose");
+const User = require("../models/user");
 
 module.exports.getAllUsers = (req, res) => {
-  User.find({}).then((user) => res.send(user)).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
+  User.find({})
+    .then((user) => res.send(user))
+    .catch((err) =>
+      res.status(500).send({ message: `Произошла ошибка${err}` })
+    );
 };
 module.exports.getUserById = (req, res) => {
   console.log(req.params.userId);
-  User.findById(req.params.userId).then((user) => { if (user == null) { res.status(404).send({ message: 'Ошибка, пользователь не найден' }); } else { res.send(user); } }).catch((err) => { if (err instanceof Mongoose.CastError) { const er = new Error(err); console.log(er); res.status(400).send({ message: er.message }); } });
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (user == null) {
+        res.status(404).send({ message: "Ошибка, пользователь не найден" });
+        return;
+      } else {
+        res.send(user);
+        return;
+      }
+    })
+    .catch((err) => {
+      if (err instanceof Mongoose.CastError) {
+        const er = new Error(err);
+        console.log(er);
+        res.status(400).send({ message: er.message });
+      }
+    });
 };
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
-  User.create([{ name, about, avatar }], { runValidators: true }).then((user) => {
-    res.send({
-      _id: user[0]._id, name: user[0].name, about: user[0].about, avatar: user[0].avatar,
+  User.create([{ name, about, avatar }], { runValidators: true })
+    .then((user) => {
+      res.send({
+        _id: user[0]._id,
+        name: user[0].name,
+        about: user[0].about,
+        avatar: user[0].avatar,
+      });
+    })
+    .catch((err) => {
+      if (err.errors.name.name == "ValidatorError") {
+        res.status(400).send({ message: err });
+        return;
+      }
+      res.status(500).send(err);
     });
-  }).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
 };
 module.exports.patchUserInfo = (req, res) => {
   const { name, about } = req.body;
@@ -27,7 +58,13 @@ module.exports.patchUserInfo = (req, res) => {
     updMaterial.about = about;
   }
   const owner = req.user._id;
-  User.findByIdAndUpdate(owner, updMaterial, { runValidators: true, new: true }).then((user) => { res.send(user); }).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
+  User.findByIdAndUpdate(owner, updMaterial, { runValidators: true, new: true })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) =>
+      res.status(400).send({ message: `Произошла ошибка${err}` })
+    );
 };
 module.exports.patchUserAvatar = (req, res) => {
   const updMaterial = {
@@ -35,5 +72,12 @@ module.exports.patchUserAvatar = (req, res) => {
   };
   const owner = req.user._id;
   console.log(owner);
-  User.findByIdAndUpdate(owner, updMaterial, { runValidators: true, new: true }).then((user) => { console.log(user); res.send(user); }).catch((err) => res.status(400).send({ message: `Произошла ошибка${err}` }));
+  User.findByIdAndUpdate(owner, updMaterial, { runValidators: true, new: true })
+    .then((user) => {
+      console.log(user);
+      res.send(user);
+    })
+    .catch((err) =>
+      res.status(400).send({ message: `Произошла ошибка${err}` })
+    );
 };
