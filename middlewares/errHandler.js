e0daaca441830dc0e2cd7f2f549768dc3bd3b031
SAaASs <andrewcraft789@gmail.com>
2023-09-20 19:16:30 +0300

    Joi fixed (maybe)

--- middlewares/errHandler.js
+++ middlewares/errHandler.js
@@ -5,7 +5,6 @@ module.exports.createUserValidator = celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().required().min(2).max(30),
-    age: Joi.number().integer().required().min(18),
     about: Joi.string().min(2).max(30),
   }),
 });
