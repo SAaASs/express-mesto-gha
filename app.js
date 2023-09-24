0baf36bcf3e5d54c55c7c371c5b35e71a0f74cda
SAaASs <andrewcraft789@gmail.com>
2023-09-24 19:02:32 +0300

    Joi fixed (maybe)23

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
+const NotFoundError = require('./errors/NotFoundError');
 require('events').EventEmitter.defaultMaxListeners = 20;
 const cookieParser = require('cookie-parser');
 const { createUser, login } = require('./controllers/users');
@@ -27,8 +28,8 @@ app.use(auth);
 
 app.use('/users/', userRouter);
 app.use('/cards/', cardsRouter);
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Выбранного пути не существует' });
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Выбранного пути не существует'));
 });
 app.use(errors());
 app.use(errHandler);
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const UnauthorisedError = require('../errors/UnauthorisedError');
 
 module.exports.auth = (req, res, next) => {
   const authorization = req.cookies.mestoAuthCookie;
   console.log(authorization);
   if (!authorization) {
     console.log('1st err');
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new UnauthorisedError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,9 +15,8 @@ module.exports.auth = (req, res, next) => {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     console.log('2nd err');
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(err);
   }
-  console.log(payload);
   req.user = payload; // записываем пейлоуд в объект запроса
 
   next(); // пропускаем запрос дальше
