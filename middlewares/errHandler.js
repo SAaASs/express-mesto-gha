74f780c3635a2a183040533520da65b219a09636
SAaASs <andrewcraft789@gmail.com>
2023-09-24 18:52:59 +0300

    Joi fixed (maybe)22

--- middlewares/errHandler.js
+++ middlewares/errHandler.js
@@ -1,5 +1,4 @@
 const { celebrate, Joi } = require('celebrate');
-const { default: mongoose } = require('mongoose');
 
 module.exports.createUserValidator = celebrate({
   body: Joi.object().keys({
@@ -7,7 +6,7 @@ module.exports.createUserValidator = celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(
-      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
     ),
     about: Joi.string().min(2).max(30),
   }),
@@ -23,21 +22,19 @@ module.exports.patchUserValidator = celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(
-      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
     ),
   }),
 });
 module.exports.createCardValidator = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    owner: Joi.string().hex().length(24),
     likes: Joi.array(),
     link: Joi.string()
       .required()
       .pattern(
-        /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+        /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
       ),
-    createdAt: Joi.date(),
   }),
 });
 module.exports.changeCardStateValidator = celebrate({
